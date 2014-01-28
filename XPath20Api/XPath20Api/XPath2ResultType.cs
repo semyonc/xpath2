0c333e05682b77c9d71fe6ffecf420ceed74dd23
Semyon A. Chertkov <semyonc@gmail.com>
2014-01-28 03:34:46 +0000

    Added XPath2ResultType.cs

---
+++ XPath20Api/XPath20Api/XPath2ResultType.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Xml;
+
+namespace Wmhelp.XPath2
+{
+    public enum XPath2ResultType
+    {
+        // Summary:
+        //     A numeric value.
+        Number = 0,
+        //
+        // Summary:
+        //     A System.String value.
+        String = 1,
+        //
+        // Summary:
+        //     A tree fragment.
+        Navigator = 1,
+        //
+        // Summary:
+        //     A System.Boolean true or false value.
+        Boolean = 2,
+        //
+        // Summary:
+        //     A node collection.
+        NodeSet = 3,
+        //
+        // Summary:
+        //     Any of the XPath node types.
+        Any = 5,
+        //
+        // Summary:
+        //     The expression does not evaluate to the correct XPath type.
+        Error = 6,
+        //
+        // Summary:
+        //     A System.DateTime value
+        DateTime = 7,
+        //
+        // Summary:
+        //     A System.DateTime value
+        Duration = 8,
+        //
+        // Summary:
+        //     A AnyUri value
+        AnyUri = 9,
+        //
+        // Summary:
+        //     A AnyUri value
+        QName = 10,
+        //
+        // Summary:
+        //     A Other typed value
+        Other = 11
+    };
+}
